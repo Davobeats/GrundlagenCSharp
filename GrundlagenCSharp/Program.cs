b0b0508eba2c303fe4d35385d96d8c426f86a898
David Waderna <david.w15@gso.schule.koeln>
2022-01-26 12:52:42 +0100

    Projektdateien hinzufügen.

---
+++ GrundlagenCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GrundlagenCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hier sind alle meine Übungen zu C#!");
+        }
+    }
+}
---
+++ SchleifenÜbungenA1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+namespace SchleifenÜbungenA1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 50; i>=0;i--)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
