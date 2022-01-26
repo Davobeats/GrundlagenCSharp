5120add196add41cb73266990eca58155a9fb93a
David Waderna <david.w15@gso.schule.koeln>
2022-01-26 13:39:41 +0100

    Vorrübergehendes Ergebnis Aufgabe2, aber keine Ausgabe

---
+++ SchleifenÜbungenA2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SchleifenÜbungenA2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //string Hans = "Ich muss immer meine Hausaufgaben machen.";
+            //for (int i = 0; i <= 50; i++) 
+            //{
+            //i = Int32.Parse(Hans);
+            //Console.WriteLine(i);
+            //}
+
+
+
+        }
+    }
+}
