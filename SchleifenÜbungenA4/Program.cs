a89fe08831a78aa5440874092033b1e67dcb0c85
David Waderna <david.w15@gso.schule.koeln>
2022-02-01 15:45:01 +0100

    Zahlen von 100 bis 10 Aufgabe.4 Schleifen Übungen

---
+++ SchleifenÜbungenA4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SchleifenÜbungenA4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i =100; i>=10; i--)
+            {
+                Console.Write(i);
+                Console.Write(" ");
+            }
+
+        }
+    }
+}
