452a8017f23dd2d22b3d06b810bd06c77986644a
David Waderna <david.w15@gso.schule.koeln>
2022-02-01 16:43:12 +0100

    Anfang Aufgabe: 5 Zahlen auswählen

---
+++ SchleifenÜbungA5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SchleifenÜbungA5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double variable = 0;
+            int auswahl = 0;
+
+            Console.WriteLine("Wählen Sie eine Positive Zahl(1) oder eine negative Zahl(2)");
+            Console.ReadLine();
+
+
+            if (auswahl == 1) ;
+            do
+            {
+                variable = variable + 1;
+                Console.Write(variable);
+            
+            } while (variable < 0) ;
+
+
+              if (auswahl == 2) ;
+              do
+              {
+                variable = variable - 1;
+                Console.Write(variable);
+              } while (variable > 0);
+
+        }
+    }
+}
