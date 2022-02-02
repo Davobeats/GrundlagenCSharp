c3d565de8bff936a82a8813c16d14978e43ee69c
David Waderna <david.w15@gso.schule.koeln>
2022-02-02 12:16:57 +0100

    Aufgabe8, Programm fertig sogar mit Feinheiten!

---
+++ SchleifenÜbungA8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SchleifenÜbungA8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pin = 1234;
+            int versuche = 0;
+
+            do
+            {
+                Console.WriteLine("PIN BESTÄTIGUNG!\nBitte geben Sie Ihren PIN ein:");
+                pin = Convert.ToInt32(Console.ReadLine());
+                if(versuche != pin)
+                {
+                    versuche = versuche + 1;
+                }
+                Console.Clear();
+            } while (pin != 1234);
+            Console.Clear();
+            Console.WriteLine("Korrekt! Sie haben {0} Versuche gebraucht!",versuche);
+
+
+        }
+    }
+}
---
+++ SchleifenÜbungenA7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SchleifenÜbungenA7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            decimal n = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("");
+
+
+
+
+
+
+
+        }
+    }
+}
