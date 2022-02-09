ca9e673674e539f98cbaf582004d5f1b161048c1
David Waderna <david.w15@gso.schule.koeln>
2022-02-09 12:54:46 +0100

    Fertig ÜbungKlausur mit Tabelle und Extra Strich

--- KlausurÜbungTabellenausgabe/Program.cs
+++ KlausurÜbungTabellenausgabe/Program.cs
@@ -6,7 +6,7 @@ namespace KlausurÜbungTabellenausgabe
     {
         static void Main(string[] args)
         {
-
+          
             double K = 0, j = 0, p = 0, bil = 0, kz = 0;
             Console.OutputEncoding = System.Text.Encoding.UTF8;
 
@@ -32,20 +32,34 @@ namespace KlausurÜbungTabellenausgabe
                 Console.Write("-");
             }
 
-            Console.WriteLine("\n");
+            Console.WriteLine();
 
             for(int Jahr = 0;Jahr <=j;Jahr++)
             {
+                
+
                 kz = kz + K * (p / 100);
                 bil = bil + K;
+                if (Jahr == j)
+                {
+                    for (int i = 0; i <= 43; i++)
+                    {
+                        Console.Write("-");
+                    }
+                    Console.WriteLine();
+                }
+
                 Console.WriteLine("{0,3}| {1,9:F2}€| {2,6:F2}€| {3,8:F2}€| {4,6:F2}€", Jahr, K, K * (p/100), kz,bil);
                 K = K + (K * (p/100));
 
 
 
-            }
-                 
 
+            }
+
+
+                 
+            
 
 
         }
