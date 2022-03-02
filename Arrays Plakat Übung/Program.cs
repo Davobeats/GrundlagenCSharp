d2ed3001a35bd292420e7a4318ba7f1525ed32dd
David Waderna <david.w15@gso.schule.koeln>
2022-03-02 12:09:28 +0100

    Übung Absolviert, Fertig

---
+++ Arrays Plakat Übung/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Arrays_Plakat_Übung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+			string[] days = new string[] { "Sun", "Mon", "Tue", "Wed", "Fri", "Sat" };
+
+
+
+			for (int i = 0; i < days.Length; i++)
+			{
+				Console.WriteLine(days[i]);
+			}
+
+
+
+		}
+	}
+}
