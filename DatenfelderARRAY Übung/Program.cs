3fb98b6d12d8f57d12845c4da56bc591b52ee49d
David Waderna <david.w15@gso.schule.koeln>
2022-03-04 12:54:25 +0100

    Ein Versuch von der Aufgabe, überhaupt nicht fertig

---
+++ DatenfelderARRAY Übung/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace DatenfelderARRAY_Übung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            int[] Note1 = new int[0];
+            int[] Note2 = new int[1];
+            int[] Note3 = new int[2];
+            int[] Note4 = new int[3];
+            int[] Note5 = new int[4];
+            int[] Note6 = new int[5];
+
+
+            Console.WriteLine("Wie viele Kinder haben die Klausur geschrieben?");
+            int eingabe = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Wie viele haben eine Eins geschrieben?");
+            Console.ReadLine();
+
+            
+            
+
+            
+
+
+
+
+            
+
+
+
+
+
+
+        }
+    }
+}
