6c12c697619255965345f655a084b6f09bb45469
David Waderna <david.w15@gso.schule.koeln>
2022-03-09 11:46:25 +0100

    Multidimensionale Arrays Übung versuch

---
+++ Mehrdimensionale Arrays Übung/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Mehrdimensionale_Arrays_Übung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[,] Array = new int[9, 5];
+
+            int Arraypos = 0;
+
+            for(int i = 0; i< Array[0,0];i++)
+            {
+                Array[0, 0] = 1 * 1;
+                Array[0, 1] = 1 * 2;
+                Array[0, 2] = 1 * 3;
+                Array[0, 3] = 1 * 4;
+                Array[0, 4] = 1 * 5;
+                
+
+                Console.WriteLine(" ");
+
+               
+
+
+
+
+            }
+            
+
+
+
+
+
+        }
+    }
+}
