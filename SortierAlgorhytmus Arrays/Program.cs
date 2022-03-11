27845816ea5ee8e6e1d99c8c26103b9ade6d9c01
David Waderna <david.w15@gso.schule.koeln>
2022-03-11 12:48:32 +0100

    Array sortierung fertig

---
+++ SortierAlgorhytmus Arrays/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SortierAlgorhytmus_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Arraysort = {45,10,15,4};
+
+            Console.WriteLine("Ihr Array unsortiert:");
+            foreach (int value in Arraysort)
+            {
+                Console.WriteLine(value + " ");
+            }
+            Console.WriteLine("Ihr Array sortiert:");
+            Array.Sort(Arraysort);
+            foreach (int value in Arraysort)
+            {
+                Console.WriteLine(value + " ");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
