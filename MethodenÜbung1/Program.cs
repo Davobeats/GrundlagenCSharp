5bc29b11b585c58b4112e185d948fe23fe7c15ea
David Waderna <david.w15@gso.schule.koeln>
2022-03-23 11:12:21 +0100

    MethodenÜbung anfang. Implementierung und aufruf

---
+++ MethodenÜbung1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MethodenÜbung1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Geben Sie etwas ein:");
+            double anzahl = Convert.ToDouble(Console.ReadLine());
+            anzahl = ausgabeTextMalX(anzahl);
+            Console.WriteLine("\nAnzahl der Buchstaben: {0}", anzahl);
+        }
+        
+        static double ausgabeTextMalX(double anzahl)
+        {
+            string name = "";
+            Console.WriteLine("Was ist ihr Name?");
+            name = Convert.ToString(Console.ReadLine());
+            for(double i = anzahl; i<=anzahl; i++)
+            {
+                Console.WriteLine(i);
+            }
+            
+            return anzahl;
+        }
+
+        
+    }
+}
