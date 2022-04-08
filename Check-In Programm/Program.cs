9af25b1763b7702ed5d1bf84834bf879ab023c44
David Waderna <david.w15@gso.schule.koeln>
2022-04-08 12:55:13 +0200

    Revert "weiter gekommen aber immernoch nicht die richtigen Ausgaben"
    This reverts commit cbe30bde45ff10165ac09d420d794d568028548f.

--- Check-In Programm/Program.cs
+++ Check-In Programm/Program.cs
@@ -14,42 +14,40 @@ namespace Check_In_Programm
             double[] rechnungsbetrag = { 82.46, 329.84, 989.52, 247.38 };
 
 
-                Console.WriteLine("Hallo. Wenn Sie mit Ihrem Nachnamen einchecken wollen, schreiben Sie irgendeinen Buchstaben.\n" +
-                "Wenn Sie mit Ihrer Zimmernummer einchecken wollen, geben Sie irgendeine Zahl ein.\n" +
-                "Sie werden weitergeleitet.");
-                string eingabe_string = Console.ReadLine();
+            Console.WriteLine("Hallo. Wenn Sie mit Ihrem Nachnamen einchecken wollen, schreiben Sie irgendeinen Buchstaben.\n" +
+            "Wenn Sie mit Ihrer Zimmernummer einchecken wollen, geben Sie irgendeine Zahl ein.\n" +
+            "Sie werden weitergeleitet.");
+            string eingabe_string = Console.ReadLine();
 
-                short eingabe_short;
-                var data = (true, 0, "", "", 0, 0.0);
+            short eingabe_short;
+            var data = (true, 0,"", "", 0, 0.0);
 
 
-                bool flag = short.TryParse(eingabe_string, out eingabe_short);
+            bool flag = short.TryParse(eingabe_string,out eingabe_short);
 
-                switch (flag)
-                {
-                    case false:
-                        data = GetData(eingabe_string, zimmernummer, vorname, nachname, übernachtungen, rechnungsbetrag);
-                        break;
+            switch (flag)
+            {
+                case false:
+                    data = GetData(eingabe_string, zimmernummer, vorname, nachname, übernachtungen, rechnungsbetrag);
+                    break;
 
-                    case true:
+                case true:
 
-                        data = GetData(eingabe_short, zimmernummer, vorname, nachname, übernachtungen, rechnungsbetrag);
-                        break;
-                }
+                    data = GetData(eingabe_short,zimmernummer,vorname,nachname,übernachtungen,rechnungsbetrag);
+                    break; 
+            }
 
-               Console.WriteLine(GetData(eingabe_string, zimmernummer, vorname, nachname, übernachtungen, rechnungsbetrag));
-
+            Console.WriteLine("Okay, {0}. Willkommen. Ihr Zimmer ist Nummer: {1}!",nachname,zimmernummer);
 
         }
+
 
         static (bool, short, string, string, short, double) GetData(short diff, short[] zn, string[] vn, string[] nn, short[] ue, double[] rb)
         {
             Console.WriteLine("Sie haben eine Zahl eingegeben und checken dadurch mit Ihrer Zimmernummer ein.\n" +
             "Welche Zimmernummer haben Sie?");
             short Zinum = Convert.ToInt16(Console.ReadLine());
-
             short index = 0;
-
             for (short i = 0; i < zn.Length; i++)
             {
                 Zinum = i;
@@ -58,29 +56,26 @@ namespace Check_In_Programm
                     index = i;
                 }
             }
-
-
-            return (false, zn[index], nn[index], vn[index], ue[index], rb[index]);
+            
+            return (true,zn[index],"","",4,8.0);
         }
 
         static (bool, short, string, string, short, double) GetData(string diff, short[] zn, string[] vn, string[] nn, short[] ue, double[] rb)
         {
             Console.WriteLine("Sie haben einen Buchstaben eingegeben und checken dadurch mit Ihrem Nachnamen ein.\n" +
             "Welchen Namen haben Sie?");
-
-            string name = Convert.ToString(Console.ReadLine());
-
+            short name = Convert.ToInt16(Console.ReadLine());
             short index = 0;
             for (short i = 0; i < zn.Length; i++)
             {
-
+                name = i;
                 if (diff == nn[i])
                 {
                     index = i;
                 }
             }
 
-            return (false, zn[index], nn[index], vn[index], ue[index], rb[index]);
+            return (true, zn[index], "", "", 4, 8.0);
         }
     }
 }
